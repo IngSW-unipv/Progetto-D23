d723075a4ba88202df47e7f98b720867c0602e59
Calogero Bugia <bugia.calo@gmail.com>
2023-05-17 17:03:30 +0200

    Attributi Persona

--- HospitalManager/src/it/unipv/sfw/Persona.java
+++ HospitalManager/src/it/unipv/sfw/Persona.java
@@ -1,5 +1,12 @@
 package it.unipv.sfw;
 
+import java.text.DateFormat;
+
 public class Persona {
+	private String nome, cognome, sesso, cf, email, cellulare, luogoNascita, indirizzo, cap, paese;
+	private DateFormat dataNascita;
+	
+	
+	
 
 }
