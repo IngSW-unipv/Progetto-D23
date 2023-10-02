d6fcda72707ee5aca7dcc478568f7b59d27fe046
AldinSubasic <aldin.subasic01@universitadipavia.it>
2023-10-02 14:52:50 +0200

    classi operatoreSanitario e operatoreUfficio, metodi e costruttori

--- HospitalManager/src/it/unipv/sfw/persona/OperatoreUfficio.java
+++ HospitalManager/src/it/unipv/sfw/persona/OperatoreUfficio.java
@@ -1,5 +1,13 @@
 package it.unipv.sfw.persona;
 
+import java.text.DateFormat;
+
 public class OperatoreUfficio extends Persona{
+
+	public OperatoreUfficio(String nome, String cognome, String CF, String sesso, String email, String cellulare,
+			String luogoNascita, String indirizzo, String cap, String paese, DateFormat dataNascita) {
+		super(nome, cognome, CF, sesso, email, cellulare, luogoNascita, indirizzo, cap, paese, dataNascita);
+		
+	}
 	
 }
