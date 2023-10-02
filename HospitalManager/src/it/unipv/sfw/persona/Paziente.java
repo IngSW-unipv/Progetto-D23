67ca543bdcda5f31ee17ef0b810260ddf83b11c8
AldinSubasic <aldin.subasic01@universitadipavia.it>
2023-10-02 14:10:11 +0200

    classe Paziente costruttore e metodi

--- HospitalManager/src/it/unipv/sfw/persona/Paziente.java
+++ HospitalManager/src/it/unipv/sfw/persona/Paziente.java
@@ -1,6 +1,38 @@
 package it.unipv.sfw.persona;
 
+import java.text.DateFormat;
+
+import it.unipv.sfw.cartellaclinica.CartellaClinica;
+
 public class Paziente extends Persona{
+
+	private int ID;
+	private CartellaClinica cartellaClinica;
+	
+	public int getID() {
+		return ID;
+	}
+	
+	public CartellaClinica getCartellaClinica() {
+		return cartellaClinica;
+	}
+	
+	public void setID(int ID) {
+		this.ID = ID;
+	}
+	
+	public void setCartellaClinica(CartellaClinica a) {
+		this.cartellaClinica = a;
+	}
+	
+	
+	public Paziente(String nome, String cognome, String CF, String sesso, String email, String cellulare,
+			String luogoNascita, String indirizzo, String cap, String paese, DateFormat dataNascita,
+			CartellaClinica cartellaClinica, int ID) {
+		super(nome, cognome, CF, sesso, email, cellulare, luogoNascita, indirizzo, cap, paese, dataNascita);
+		this.cartellaClinica = cartellaClinica;
+		this.ID = ID;
+	}
 	
 
 }
