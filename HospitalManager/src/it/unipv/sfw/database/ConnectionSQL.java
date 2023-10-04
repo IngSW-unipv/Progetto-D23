903223f313927d3997db60b0ee297bc69ae644a3
AldinSubasic <aldin.subasic01@universitadipavia.it>
2023-10-04 10:52:06 +0200

    classe per connettere al server SQL locale

---
+++ HospitalManager/src/it/unipv/sfw/database/ConnectionSQL.java
@@ -0,0 +1,69 @@
+package it.unipv.sfw.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+/**
+ * Serve per connettersi a database.
+ */
+
+public class ConnectionSQL {
+	/**
+	 * inizializza connessione col db
+	 */
+	public static Connection startConnection(Connection conn, String schema) {
+
+		String DbURL = null;
+		String username = null;
+		String password = null;
+
+		DbURL = "";
+		username = "";
+		password = "";
+		
+		if (isOpen(conn))
+			closeConnection(conn);
+		
+		try {
+
+			Class.forName("com.mysql.cj.jdbc.Driver");
+
+			conn = DriverManager.getConnection(DbURL, username, password);// Apertura connessione
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+		return conn;
+	}
+
+	/**
+	 * restituisce true se connessione db è aperta
+	 */
+	public static boolean isOpen(Connection conn) {
+		if (conn == null)
+			return false;
+		else
+			return true;
+	}
+
+	/**
+	 * chiude connessione col db
+	 */
+	public static Connection closeConnection(Connection conn) {
+		if (!isOpen(conn))
+			return null;
+		try {
+
+			conn.close();
+			conn = null;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+		return conn;
+	}
+}
