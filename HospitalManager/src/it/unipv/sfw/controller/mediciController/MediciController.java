1408619795121379024bb9e803f6e7fef2d133f4
AldinSubasic <aldin.subasic01@universitadipavia.it>
2023-10-07 16:21:41 +0200

    prima bozza del controller dei medici

---
+++ HospitalManager/src/it/unipv/sfw/controller/mediciController/MediciController.java
@@ -0,0 +1,11 @@
+package it.unipv.sfw.controller.mediciController;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import it.unipv.sfw.strutturasanitaria.StrutturaSanitaria; //modello
+
+
+public class MediciController {
+
+}
