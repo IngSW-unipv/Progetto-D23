75b3d954eea2e4162615bd06aaa2eb1310464e48
Calogero Bugia <bugia.calo@gmail.com>
2023-11-28 17:27:01 +0100

    Aggiunto Panel Cambia Password

---
+++ HospitalManager/src/it/unipv/sfw/view/login/CambiaPwPanel.java
@@ -0,0 +1,33 @@
+package it.unipv.sfw.view.login;
+
+import java.awt.GridLayout;
+
+import javax.swing.*;
+
+public class CambiaPwPanel extends JPanel{
+	
+	private JLabel vecchiaPw, nuovaPw;
+	private JPasswordField oldPw, newPw;
+	private JButton conferma, indietro;
+
+	public CambiaPwPanel() {
+		
+		setLayout(new GridLayout(3,3));
+		
+		oldPw = new JPasswordField();
+		newPw = new JPasswordField();
+		conferma = new JButton("CONFERMA PASSWORD");
+		vecchiaPw = new JLabel("DIGITA VECCHIA PASSWORD");
+		nuovaPw = new JLabel("DIGITA NUOVA PASSWORD");
+		indietro = new JButton("INDIETRO");
+		
+		add(vecchiaPw);
+		add(oldPw);
+		add(nuovaPw);
+		add(newPw);
+		add(indietro);
+		add(conferma);
+		
+	}
+
+}
